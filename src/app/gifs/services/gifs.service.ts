c9837bb2d75b90d3787aa7a65cdc7b8d39b9840d
David Martínez <dmmtapia.ux@gmail.com>
2024-09-11 09:00:11 -0300

    changes

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,23 +1,34 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({ providedIn: 'root' })
-
-
 export class GifsService {
-  private _tagHistory: string[] = [];
+  private _tagsHistory: string[] = [];
   constructor() {}
 
   get tagsHistory() {
-    return [...this._tagHistory];
+    return [...this._tagsHistory];
+  }
+
+  private organizeHistory(tag: string) {
+    //guardar en minuscula en el arreglo de historial de búqueda
+    tag = tag.toLowerCase();
+
+    //areglo con todos los tag
+    if (this._tagsHistory.includes(tag)) {
+      this._tagsHistory = this._tagsHistory.filter((oldTag) => oldTag !== tag);
+    }
+
+
+    this._tagsHistory.unshift(tag);
+    // mantener el arreglo limitado a 10
+    this._tagsHistory = this.tagsHistory.splice(0, 10);
   }
 
   public searchTag(tag: string): void {
-    this._tagHistory.unshift(tag);
+    //no devolvera nada el input si el campo esta vacio
+    if (tag.length === 0) return;
+    this.organizeHistory(tag);
+
+    // this._tagsHistory.unshift(tag);
   }
 }
-
-
-
-
-
-
